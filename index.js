0ece982b5d8e1fbd2ec0bae32957cadbc5966360
Guy Bianco IV <gbianco@redhat.com>
2015-08-10 21:31:42 -0400

    [#10] process command line args

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 #! /usr/bin/env node
-
 'use strict';
 
-console.log('stupid simple dotfiles manager');
+var args = require('minimist')(process.argv.slice(2), {
+  alias: {
+    h: 'help',
+    v: 'version'
+  }
+});
+
+if (args.help) {
+  printHelp();
+}
+
+if (args.version) {
+  console.log(require('./package.json').version);
+  process.exit();
+}
+
+// else
+printHelp();
+
+function printHelp() {
+  var cmd = require('path').basename(process.argv[1]);
+  console.log(
+    require('fs')
+      .readFileSync(__dirname+'/res/help.txt', 'utf-8')
+      .replace(/\$0/g, cmd)
+      .trim()
+  );
+  process.exit();
+}
