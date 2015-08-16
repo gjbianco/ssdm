98968a64154d8e7e8e92fcdefe02e2a6ede180fa
Guy Bianco IV <gbianco@redhat.com>
2015-08-15 21:22:21 -0400

    addfile and list tests

--- test/test.js
+++ test/test.js
@@ -3,8 +3,10 @@
 var fs = require('fs');
 var expect = require('chai').expect;
 var ssdm = require('../lib/ssdm.js');
+require('shelljs/global');
 
-describe ('#ssdm', function () {
+describe ('#ssdm-basic', function () {
+
   it ('should display usage on no command', function () {
     var result = ssdm ();
     var help = fs
@@ -12,5 +14,63 @@ describe ('#ssdm', function () {
                 .replace(/\$0/g, 'ssdm')
                 .trim();
     expect(result).to.equal(help);
-  })
+  });
+
 });
+
+describe('#ssdm-util', function () {
+
+  beforeEach(_setUpTestDir);
+
+  it ('should list whitelisted files for list', function () {
+    var cmdResponse = ssdm(['list']);
+    var expected = '*\n!.gitignore';
+    expect(cmdResponse).to.equal(expected);
+  });
+
+  afterEach(_tearDownTestDir);
+
+});
+
+describe('#ssdm-adding', function () {
+
+  beforeEach(_setUpTestDir);
+
+  it ('should add file pattern to whitelist', function () {
+    var cmdResponse = ssdm (['addfile', '.bashrc']);
+    var result = fs.readFileSync('.ssdmignore').toString();
+    var expected = '*\n!.gitignore\n!.bashrc';
+    expect(result).to.equal(expected);
+  });
+
+  it ('should add multiple file patterns to whitelist', function () {
+    var cmdResponse = ssdm (['addfile', '.bashrc', '.vimrc', '.gitconfig']);
+    var result = fs.readFileSync('.ssdmignore').toString();
+    var expected = '*\n!.gitignore\n!.bashrc\n!.vimrc\n!.gitconfig';
+    expect(result).to.equal(expected);
+  });
+
+  afterEach(_tearDownTestDir);
+
+});
+
+function _setUpTestDir() {
+    mkdir('test-dir');
+    cd('test-dir');
+    if (exec('git init', {silent: true}).code !== 0) {
+      echo('Error: \'git init\' failed');
+      return 1;
+    } else {
+      mv('.git', '.ssdm');
+    }
+    '*\n!.gitignore'.to('.ssdmignore');
+}
+
+function _tearDownTestDir() {
+    // make sure we are in test-dir before we start changing dirs
+    var testDirPattern = /.*\/test-dir\/?$/;
+    if (testDirPattern.test(process.cwd())) {
+      cd('..');
+    }
+    rm('-rf', 'test-dir');
+}
